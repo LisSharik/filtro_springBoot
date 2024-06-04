69f56b72e13de549460fe87e98f5789ea2f0e2ec
lisSharik <lissharik@gmail.com>
2024-06-04 07:07:26 -0500

    Relaciones survey y question

--- filtro_spring_boot/src/main/java/com/riwi/filtro_spring_boot/domain/entities/Question.java
+++ filtro_spring_boot/src/main/java/com/riwi/filtro_spring_boot/domain/entities/Question.java
@@ -1,13 +1,11 @@
 package com.riwi.filtro_spring_boot.domain.entities;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.riwi.filtro_spring_boot.util.enums.QuestionType;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,42 +13,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Entity(name = "survey")
+@Entity(name = "question")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Survey {
+public class Question {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
-    @Column(nullable = false, length = 255)
-    private String title;
-
     @Lob
-    private String description;
+    @Column(name = "text")
+    private String text;
 
-    @CreationTimestamp
-    private LocalDate creationDate;
+    @Enumerated(EnumType.STRING)
+    private QuestionType type;
 
     @Column(nullable = false)
     private Boolean active;
 
-
-    @OneToMany(mappedBy = "question", fetch = FetchType.EAGER,
-    cascade = CascadeType.ALL, orphanRemoval = false)
-    private List<Question> questions;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "creator_id", referencedColumnName = "id")
-    private UserEntity creator;
+    @JoinColumn(name = "survey_id", referencedColumnName = "id")
+    private Survey survey;
+
 
 }
